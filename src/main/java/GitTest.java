88606c3692f7ecf376c8f08c35d15263acfe7f8f
msffn <111100529+msffn@users.noreply.github.com>
2022-08-12 10:45:09 +0800

    Create GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("master git");
         System.out.println("two git");
+        System.out.println("hello github ");
     }
 }
